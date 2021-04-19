1e685707a1dc83356ae5fb10a75202c236a38572
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-04-19 21:50:04 +0300

    Can be added the new consumer

--- app/controllers/consumers_controller.rb
+++ app/controllers/consumers_controller.rb
@@ -2,4 +2,27 @@ class ConsumersController < ApplicationController
   def index
     @consumers=Consumer.order(:name)
   end
+
+  def new
+    @consumer = Consumer.new
+  end
+
+  def create
+    @consumer = Consumer.new(consumer_params)
+    if @consumer.save
+      redirect_to consumers_path, notice: "Споживач #{@consumer.name} успішно створений"
+    else
+      flash[:alert] = 'Неможливо створити споживача'
+      render :new
+    end
+  end
+
+  private
+
+  def consumer_params
+    params.require(:consumer).permit(:name, :full_name, :edrpou, :inn, :director,
+                                     :bank, :mfo, :account, :address, :phone, :mail,
+                                     :dog_en_number, :dog_en_date, :distribution,
+                                     :client_username, :manager_username, :onec_id)
+  end
 end

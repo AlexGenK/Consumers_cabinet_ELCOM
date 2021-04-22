167c55e80d1d87e030b1cb953c99a8762ea6826f
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-04-22 23:09:32 +0300

    The purveyor can be editing

--- app/controllers/admin/purveyor_controller.rb
+++ app/controllers/admin/purveyor_controller.rb
@@ -1,4 +1,29 @@
 class Admin::PurveyorController < ApplicationController
+	before_action :set_purveyor
+
 	def edit
 	end
+
+  def update
+    if @purveyor.update(purveyor_params)
+      flash[:notice] = "Реквізити постачальника відредаговані" 
+      redirect_to consumers_path
+    else
+      render :edit
+    end
+  end
+
+
+	private
+
+	def set_purveyor
+    @purveyor = Purveyor.first
+    @purveyor ||= Purveyor.create(full_name: '')
+  end
+
+  def purveyor_params
+  	params.require(:purveyor).permit(:full_name, :director, :edrpou, :inn,
+  																	 :account, :bank, :mfo, :address, 
+  																	 :phone, :mail)
+  end
 end

1ae76a6dd4582d0cfa86826c081ad3b95aad5efa
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-05-05 01:07:23 +0300

    Added ukrainian locale

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ConsumersCabinetElcom
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.i18n.default_locale = :uk
 
     # Configuration for the application, engines, and railties goes here.
     #

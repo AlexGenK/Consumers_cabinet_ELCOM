596aa00ef33f5eb9ff150cb535d41549c0caccb9
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-05-05 19:40:36 +0300

    A new user can be added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,10 @@ class UsersController < ApplicationController
 	def index
     @users = User.order(:name)
   end
-  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:admin_role, :manager_role, :client_role, :name, :email)
+  end
 end

66e40a2df66117bf8567ae25e887474863494593
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-05-26 20:51:19 +0300

    Fix loading certificates of consumption

--- app/controllers/admin/filling_ccertificates_controller.rb
+++ app/controllers/admin/filling_ccertificates_controller.rb
@@ -12,21 +12,22 @@ class Admin::FillingCcertificatesController < ApplicationController
                   port: ENV['CONSUMERS_CABINET_ELCOM_FTP_PORT'],
                   username: ENV['CONSUMERS_CABINET_ELCOM_FTP_USERNAME'],
                   password: ENV['CONSUMERS_CABINET_ELCOM_FTP_PASSWORD'],) do |ftp|
-      ftp.chdir('c')
       ftp.nlst.each do |filename|
-        @consumer = Consumer.find_by(onec_id: parse_id(filename))
-        if @consumer
-            @cert_date = parse_date(filename)
-            @consumption = @consumer.consumptions.where('extract(year from period) = ? AND extract(month from period) = ?', @cert_date.year, @cert_date.month).first
-            if @consumption
-              @consumption.ccertificate&.destroy
-              @cert_number = parse_number(filename)
-              @cert = @consumption.build_ccertificate(number: @cert_number, date: @cert_date)
-              @cert.save!
-              ftp.getbinaryfile(filename, 'public/akt.pdf')
-              @cert.print_form.attach(io: File.open('public/akt.pdf'), filename: 'akt.pdf', content_type: 'application/pdf')
-              @add_certs << "Споживач #{@consumer.name} (#{@consumer.edrpou}) - Акт №#{@cert.number} від #{@cert.date}"
-            end
+        if filename[0] == 'C'
+          @consumer = Consumer.find_by(onec_id: parse_id(filename))
+          if @consumer
+              @cert_date = parse_date(filename)
+              @consumption = @consumer.consumptions.where('extract(year from period) = ? AND extract(month from period) = ?', @cert_date.year, @cert_date.month).first
+              if @consumption
+                @consumption.ccertificate&.destroy
+                @cert_number = parse_number(filename)
+                @cert = @consumption.build_ccertificate(number: @cert_number, date: @cert_date)
+                @cert.save!
+                ftp.getbinaryfile(filename, 'public/akt.pdf')
+                @cert.print_form.attach(io: File.open('public/akt.pdf'), filename: 'akt.pdf', content_type: 'application/pdf')
+                @add_certs << "Споживач #{@consumer.name} (#{@consumer.edrpou}) - Акт №#{@cert.number} від #{@cert.date}"
+              end
+          end
         end
       end
     end
--- app/controllers/concerns/parsed.rb
+++ app/controllers/concerns/parsed.rb
@@ -2,17 +2,17 @@ module Parsed
   extend ActiveSupport::Concern
 
   def parse_id(filename)
-    filename =~ /(D|C)(\d+)_\d{1,2}-\d{1,2}-\d{1,2}_\d+/
+    filename =~ /(D|C)(\d+)_\d{2}\.\d{2}\.\d{2}_\D{2}-\d+/
     $2
   end
 
   def parse_date(filename)
-    filename =~ /(D|C)\d+_(\d{1,2}-\d{1,2}-\d{1,2})_\d+/
-    Date.strptime($2,'%y-%m-%d')
+    filename =~ /(D|C)\d+_(\d{2}\.\d{2}\.\d{2})_\D{2}-\d+/
+    Date.strptime($2,'%d.%m.%y')
   end
 
   def parse_number(filename)
-    filename =~ /(D|C)\d+_\d{1,2}-\d{1,2}-\d{1,2}_(\d+)/
+    filename =~ /(D|C)\d+_\d{1,2}\.\d{1,2}\.\d{1,2}_\D{2}-(\d+)/
     $2
   end
 end

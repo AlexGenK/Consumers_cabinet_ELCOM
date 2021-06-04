6ec27fed3b14222a29fb357a59a5be8e5b273d9c
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-06-04 08:49:36 +0300

    Import .csv files with double quotes

--- app/services/read_csv_file_service.rb
+++ app/services/read_csv_file_service.rb
@@ -3,8 +3,8 @@ class ReadCsvFileService
 
   def self.call(file, structure)
     @a = []
-    @csv_text = file.read.encode('UTF-8', 'Windows-1251', invalid: :replace, indef: :replace).gsub(/"/,'\'')
-    @csv = CSV.parse(@csv_text, col_sep: ';')
+    @csv_text = file.read.encode('UTF-8', 'Windows-1251', invalid: :replace, indef: :replace)
+    @csv = CSV.parse(@csv_text, col_sep: ';', quote_char: "|")
     @csv.each do |item|
       @a << Hash[structure.each_with_index.map {|x, i| [x, item[i]]}]
     end
